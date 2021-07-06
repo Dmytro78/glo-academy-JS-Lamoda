f0a2e21c1b5e7530ed3195f413be8f8e2df058f3
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-07-06 14:02:56 +0300

    modal window

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,37 @@ if (localStorage.getItem('Lomoda-Location')) {
     headerCityButton.textContent = localStorage.getItem('Lomoda-Location');
 }
 */
-headerCityButton.textContent = localStorage.getItem('Lomoda-Location') || 'Ваш город?';
-
+headerCityButton.textContent = localStorage.getItem('Lomoda-Location') || "Ваш город?";
 
 headerCityButton.addEventListener('click', () => {
     const city = prompt('Укажите Ваш город');
     headerCityButton.textContent = city;
     localStorage.setItem('Lomoda-Location', city);
+});
+
+
+// modal window
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+};
+
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', e => {
+    const target = e.target;
+    /*
+    if (target.classList.contains('cart__btn-close')) {cartModalClose();}
+    */
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
 });
