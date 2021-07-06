de17fc5c969491fd0e06cae4def2c1d096b7ac5d
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-07-06 16:01:59 +0300

    scroll

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import "./js/city.js";
 import "./js/modal-basket.js";
-import "./js/scroll-lock.js";
+// import "./js/scroll-lock.js";
--- js/modal-basket.js
+++ js/modal-basket.js
@@ -5,11 +5,13 @@ const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
     cartOverlay.classList.add('cart-overlay-open');
+     disableScroll();
 };
 
 const cartModalClose = () => {
     cartOverlay.classList.remove('cart-overlay-open');
     window.addEventListener("keydown", closeEsc);
+    enableScroll();
 };
 
 
@@ -25,8 +27,37 @@ cartOverlay.addEventListener('click', e => {
     }
 });
 
+// close Esc
+
 function closeEsc(e) {
     if (e.code === "Escape") {
         cartModalClose(e);
     }
 }
+
+// scroll-lock
+
+/*
+const disableScroll = () => { document.body.style.overflow = 'hidden'; };
+const enableScroll = () => { document.body.style.overflow = '';};
+*/
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    document.body.dbScrollY = window.scrollY;
+    document.body.style.cssText = `
+        position: fixed;
+        top: ${-window.scrollY}px;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `;
+};
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY
+    });
+};
